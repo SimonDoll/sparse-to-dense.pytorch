a2901564d9b58b91772892af8c2159917ef15300
Simon D <simondoll.studium@gmail.com>
2021-04-17 10:19:25 +0000

    updates requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='sparse_to_dense', version='1.0', packages=['sparse_to_dense'],)
+setup(name='sparse_to_dense',
+      version='1.0',
+      packages=['sparse_to_dense'],
+      install_requires=[
+          'matplotlib',
+          'h5py',
+          'imageio',
+          'opencv-python',
+      ],
+      )
